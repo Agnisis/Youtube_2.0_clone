c1c716994f4d771c9cdabe5a6dd821f0875dfa74
Agnisis Dutta <agnisisdutta16@gmail.com>
2023-08-30 22:52:13 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-4xl text-red-600 font-bold'>Youtube clone 2.0</h1>
+    </div>
+  )
+}
+
+export default App
+
